a063909e3e5dd749f6955fbd08c5d92dd785a4c1
Dhruv Pancholi <pancholidhruv@gmail.com>
2015-05-12 19:30:37 +0530

    Update keyset.php
    Removed unwanted keys

--- hallabol/keyset.php
+++ hallabol/keyset.php
@@ -10,17 +10,6 @@ if(!$db_server) die("Unable to connect to MySQL: ".mysql_error());
 mysql_select_db($db_database) or die("Unable to select database: ".mysql_error());
 
 function run_query($query){
-/*	if(isset($db_server)){
-	$db_hostname = 'localhost';
-	$db_database = 'students_test';
-	$db_username = 'students_test';
-	$db_password = 'secret';
-
-	$db_server = mysql_connect($db_hostname, $db_username, $db_password);
-	if(!$db_server) die("Unable to connect to MySQL: ".mysql_error());
-	mysql_select_db($db_database) or die("Unable to select database: ".mysql_error());
-	}
-*/
 	$result = mysql_query($query);
 	if(!$result) die("Database access failed: ".mysql_error());
 	return $result;
